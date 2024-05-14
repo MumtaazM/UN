615fbe21d5422b1e84cbdb795cd1ed3e12df6614
Mumtaaz <64772468+MumtaazM@users.noreply.github.com>
2024-05-14 14:17:13 -0400

    Initial upload

---
+++ src/main/java/com/union_notes/union_notes/UnionNotesApplication.java
@@ -0,0 +1,13 @@
+package com.union_notes.union_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnionNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/union_notes/union_notes/UnionNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.union_notes.union_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
