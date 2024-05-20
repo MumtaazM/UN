9e7093dd3f82ad38acb518e1be1801a6704e78e6
Mumtaaz <64772468+MumtaazM@users.noreply.github.com>
2024-05-19 23:27:01 -0400

    added frontend url to config file

--- api/src/main/java/com/union_notes/union_notes/WebConfig.java
+++ api/src/main/java/com/union_notes/union_notes/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // replace with your frontend domain
+                .allowedOrigins("https://union-notes.pages.dev/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
