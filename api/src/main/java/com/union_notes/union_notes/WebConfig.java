5e45eb830b8f4e380ffd0e0f902cc56d838d0933
Mumtaaz <64772468+MumtaazM@users.noreply.github.com>
2024-05-19 23:31:08 -0400

    fixed config class

--- api/src/main/java/com/union_notes/union_notes/WebConfig.java
+++ api/src/main/java/com/union_notes/union_notes/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://union-notes.pages.dev/")
+                .allowedOrigins("https://union-notes.pages.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
