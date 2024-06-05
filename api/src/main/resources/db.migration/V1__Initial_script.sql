5d3a29fd204d3b13cdffab273e9cdf1ddb690c09
Mumtaaz <64772468+MumtaazM@users.noreply.github.com>
2024-06-05 13:53:49 -0400

    updated config values

--- api/src/main/resources/db.migration/V1__Initial_script.sql
+++ api/src/main/resources/db.migration/V1__Initial_script.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    role VARCHAR(255) DEFAULT 'USER'
 );
 
 CREATE TABLE task (
